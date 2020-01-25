1f97cbd8adea0e2f60cd1144dc1ddfdf398ea7e3
Miikka Tuominen <tuommii@gmail.com>
2020-01-25 23:32:51 +0200

    back to golang

--- main.go
+++ main.go
@@ -10,73 +10,61 @@ import (
 
 // Player represents player
 type Player struct {
-	Name   string
-	Points int
+	Score      int `json:"score"`
+	ClicksLeft int `json:"clicksLeft"`
 }
 
-// Rulebook represents all game rule
-type Rulebook struct {
-	// [click_count]prize_points
-	prizes  map[int]int
-	players map[string]Player
-}
+var gClicks int
+var gPlayers map[string]*Player
 
-// CreateRulebook ...
-func CreateRulebook() Rulebook {
-	rb := Rulebook{}
-	rb.prizes = make(map[int]int)
-	rb.players = make(map[string]Player)
-	rb.prizes[10] = 5
-	rb.prizes[100] = 40
-	rb.prizes[250] = 500
-	return rb
-}
-
-// CreatePlayer creates new player
-func CreatePlayer() Player {
-	// TODO: Maybe static int player_count, and add to it player name
-	p := Player{Points: 20, Name: "Default"}
-	return p
-}
-
+// [::1]:49046
 func parseIP(addr string) string {
 	arr := strings.Split(addr, " ")
-	return arr[len(arr)-1]
+	ip := strings.Split(arr[len(arr)-1], "]")
+	return ip[0]
 }
 
-func dataHandle(w http.ResponseWriter, r *http.Request) {
-	// response := struct {
-	// 	Players []model.Player
-	// 	Games   []model.Game
-	// 	Message string
-	// }{
-	// 	players,
-	// 	games,
-	// 	msg,
-	// }
-	//s.templates["home.html"].ExecuteTemplate(w, "base", response)
-	resp := struct {
-		Player
-	}{
-		CreatePlayer(),
+func init() {
+	gPlayers = make(map[string]*Player)
+	gClicks = 0
+}
+
+func getState(w http.ResponseWriter, r *http.Request) {
+	ip := parseIP(r.RemoteAddr)
+	if _, ok := gPlayers[ip]; ok {
+		// Player exist
+	} else {
+		p := &Player{Score: 0, ClicksLeft: 20}
+		gPlayers[ip] = p
 	}
-	// 2020/01/25 12:01:52 10.12.5.12:50995
-	log.Printf("%v\n", r.RemoteAddr)
-	log.Println(parseIP(r.RemoteAddr))
 	w.Header().Set("Content-Type", "application/json")
-	json, _ := json.Marshal(resp)
-	w.Write(json)
+	log.Println(gPlayers[ip])
+	json.NewEncoder(w).Encode(gPlayers)
+}
+
+// TODO: Handle Reset
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	ip := parseIP(r.RemoteAddr)
+	// Check prizes
+	if (gClicks+1)%10 == 0 {
+		gPlayers[ip].Score = 10
+	}
+	gPlayers[ip].ClicksLeft--
+	gClicks++
+	log.Println(ip, gPlayers[ip])
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(gPlayers)
 }
 
 func main() {
 	port := os.Getenv("PORT")
-	// rb := CreateRulebook()
 	if port == "" {
 		port = "3000"
 	}
 	fs := http.FileServer(http.Dir("./public"))
+	http.HandleFunc("/inc", incrementCounter)
+	http.HandleFunc("/state", getState)
 	http.Handle("/", fs)
-	http.HandleFunc("/data", dataHandle)
 	err := http.ListenAndServe("0.0.0.0:"+port, nil)
 	if err != nil {
 		log.Fatal(err)
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,33 @@
-console.log('Hello!');
+// console.log('Hello!');
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    clicksLeft: 1,
+    total: 2,
+    fetched: false
+  },
+  methods: {
+    initData: function(url) {
+      const req = new Request(url);
+      fetch(req)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        a = Object.keys(data)[0]
+        this.clicksLeft = data[a].clicksLeft;;
+        console.log(a);
+        this.fetched = true;
+      });
+    },
+    handleClick: function() {
+      this.initData('http://localhost:3000/inc')
+    }
+  },
+  created: function() {
+    this.initData('http://localhost:3000/state');
+  }
+})
