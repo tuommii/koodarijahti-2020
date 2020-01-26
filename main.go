8114305aea639bc7e2bde139148452249f2b6943
Miikka Tuominen <tuommii@gmail.com>
2020-01-26 19:08:12 +0200

    env check

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ type State struct {
 	Players   map[string]*Player
 	NextPrize int
 	Prizes    map[int]int
+	Env       string
 }
 
 func (s *State) checkPrize(ip string) {
@@ -57,12 +58,22 @@ func parseIP(addr string) string {
 	return str
 }
 
+func (s *State) getIP(w http.ResponseWriter, r *http.Request) string {
+	var ip string
+	w.Header().Set("Content-Type", "application/json")
+	if s.Env != "production" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		ip = parseIP(r.RemoteAddr)
+	} else {
+		ip = r.Header.Get("X-Forwarded-For")
+	}
+	return ip
+}
+
 func (s *State) getState(w http.ResponseWriter, r *http.Request) {
 	var p *Player
-	fwd := r.Header.Get("fwd")
-	fwd2 := r.Header.Get("X-Forwarded-For")
-	log.Println("HEADER1:", fwd, fwd2)
-	ip := parseIP(r.RemoteAddr)
+	// fwd := r.Header.Get("fwd")
+	ip := s.getIP(w, r)
 	if _, ok := s.Players[ip]; ok {
 		// Player exist, take data form State
 		p = &Player{
@@ -75,15 +86,13 @@ func (s *State) getState(w http.ResponseWriter, r *http.Request) {
 		s.Players[ip] = p
 		s.Players[ip].NextPrize = s.NextPrize
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	log.Println("GET STATE:", s.Players[ip], r.RemoteAddr)
+	log.Println("GET STATE:", s.Players[ip], ip)
 	json.NewEncoder(w).Encode(s.Players[ip])
 }
 
 // Handle
 func (s *State) handleAction(w http.ResponseWriter, r *http.Request) {
-	ip := parseIP(r.RemoteAddr)
+	ip := s.getIP(w, r)
 
 	if s.Players[ip].ClicksLeft == 0 {
 		json.NewEncoder(w).Encode(s.Players[ip])
@@ -95,24 +104,25 @@ func (s *State) handleAction(w http.ResponseWriter, r *http.Request) {
 	s.checkPrize(ip)
 	s.Players[ip].NextPrize = s.NextPrize
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	log.Println("ACTION:", r.RemoteAddr)
+	log.Println("ACTION:", s.Players[ip], ip)
 	json.NewEncoder(w).Encode(s.Players[ip])
 }
 
 func main() {
-	state := &State{Clicks: 0, NextPrize: 10}
+	state := &State{Clicks: 0, NextPrize: 10, Env: "dev"}
 	state.Players = make(map[string]*Player)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
+	} else {
+		state.Env = "production"
 	}
 	fs := http.FileServer(http.Dir("./public"))
 	http.HandleFunc("/action", state.handleAction)
 	http.HandleFunc("/state", state.getState)
 	http.Handle("/", fs)
+	log.Println("Server started on", state.Env)
 	err := http.ListenAndServe("0.0.0.0:"+port, nil)
 	if err != nil {
 		log.Fatal(err)
