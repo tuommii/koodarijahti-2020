29f0d24781e1c0aa4f4571f49cba6d8d58fd2422
Miikka Tuominen <tuommii@gmail.com>
2020-01-27 09:53:49 +0200

    more ui

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 // console.log('Hello!');
 
-const audio = document.getElementById('audio-click');
+const prizeAudio = document.getElementById('audio-prize');
+const clickAudio = document.getElementById('audio-click');
 
 const STARTING_POINTS = 20;
 
@@ -27,10 +28,7 @@ var app = new Vue({
       .then((data) => {
         console.log(data);
         this.clicksLeft = data.clicksLeft;
-        if (this.score != data.score && url === "/click")
-          this.message = `You won ${data.score-this.score} points!`;
-        else
-          this.message = '';
+        this.showMessage(data, url);
         this.score = data.score
         if (this.clicksLeft != STARTING_POINTS) {
           this.nextPrize = data.nextPrize;
@@ -43,15 +41,28 @@ var app = new Vue({
       e.preventDefault();
       // TODO: Change this
       if (this.clicksLeft || !started) {
-        audio.currentTime = 0;
-        audio.play();
+        clickAudio.currentTime = 0;
+        clickAudio.play();
         this.isDisabled = true;
         window.setTimeout(() => {
           console.log(this.isDisabled);
           this.isDisabled = false;
-        }, 500);
+        }, 250);
         this.fetchData('/click')
       }
+    },
+    showMessage: function(data, url) {
+      if (!this.clicksLeft) {
+        this.message = `gg! You scored ${data.score} points!`;
+      }
+      else if (this.score != data.score && url === "/click") {
+        prizeAudio.currentTime = 0;
+        prizeAudio.play();
+        this.message = `You won ${data.score-this.score} points!`;
+      }
+      else {
+        this.message = '';
+       }
     },
     reset: function(e) {
       e.preventDefault();
