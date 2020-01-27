3f1d677d46359e4e14895013917525ac535ca2f1
Miikka Tuominen <tuommii@gmail.com>
2020-01-27 11:57:09 +0200

    bad bug maybe fixed

--- handles.go
+++ handles.go
@@ -16,8 +16,8 @@ func (gs *GameState) handleGetState(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// Add new player
 		p = createPlayer(StartingPoints, gs.NextPrize)
-		gs.Players[ip] = p
 	}
+	gs.Players[ip] = p
 	log.Println("/STATE:", gs.Players[ip], ip, gs.Clicks)
 	json.NewEncoder(w).Encode(gs.Players[ip])
 }
@@ -25,15 +25,13 @@ func (gs *GameState) handleGetState(w http.ResponseWriter, r *http.Request) {
 // Update's game state
 func (gs *GameState) handleClick(w http.ResponseWriter, r *http.Request) {
 	ip := gs.getIP(w, r)
-	player := gs.Players[ip]
-	log.Println("IP:", ip, player)
 	if gs.Players[ip].Points == 0 {
 		json.NewEncoder(w).Encode(gs.Players[ip])
-	} else {
-		gs.update(ip)
-		log.Println("/ACTION:", gs.Players[ip], ip, gs.Clicks)
-		json.NewEncoder(w).Encode(gs.Players[ip])
+		return
 	}
+	gs.update(ip)
+	log.Println("/ACTION:", gs.Players[ip], ip, gs.Clicks)
+	json.NewEncoder(w).Encode(gs.Players[ip])
 }
 
 // Reset player's data
@@ -43,6 +41,7 @@ func (gs *GameState) handleReset(w http.ResponseWriter, r *http.Request) {
 		gs.Players[ip].Points = StartingPoints
 		gs.Players[ip].NextPrize = gs.NextPrize
 		json.NewEncoder(w).Encode(gs.Players[ip])
+		return
 	}
 	log.Println("/RESET:", gs.Players[ip], ip)
 	json.NewEncoder(w).Encode(gs.Players[ip])
